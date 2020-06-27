73609359bc0359e063c2edde38011b9997161842
Kaiqi Yang <kaiqi.yang@afterpay.com>
2020-06-27 17:24:00 +1000

    corn test: baseline

---
+++ cron-test/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile = "saml"
+  region  = "ap-southeast-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-b374d5a5"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
